5639b4e425f2ea8d1b3056e3dac431457b5e24bc
Diego Moura Torres <95168133+dieggo-torres@users.noreply.github.com>
2022-06-14 09:46:49 -0300

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+
+function App() {
+  let [count, setCount] = useState(0);
+
+  function increaseCount() {
+    setCount(count + 1);
+  }
+
+  function decreaseCount() {
+    setCount(count - 1);
+  }
+
+  return (
+    <div className="container">
+      <h1>{count}</h1>
+      <button onClick={increaseCount}>+</button>
+      <button onClick={decreaseCount}>-</button>
+    </div>
+  );
+}
+
+export default App;
